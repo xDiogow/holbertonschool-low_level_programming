633bb843aff160ca5c4862fc68c768aee0179004
Diogo <diogoanonimos@gmail.com>
2025-02-17 14:54:45 +0100

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,8 @@ void print_alphabet_x10(void)
 	char ch;
 	int i;
 
-	for (i = 0; i < 10; i++) {
+	for (i = 0; i < 10; i++)
+	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
 		_putchar('\n');

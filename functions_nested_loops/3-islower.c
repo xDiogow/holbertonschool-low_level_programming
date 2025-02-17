8de70d6b965c4e54afde702ff53fe758e88ec66d
Diogo <diogoanonimos@gmail.com>
2025-02-17 15:12:35 +0100

    3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * islower - Checks if a letter is lowercase.
+ *
+ * Return: 1 when letter is lowercase
+ *         0 when letter is uppercase
+ */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)

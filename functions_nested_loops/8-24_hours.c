53f04d6dcb2fbd3cf05edab23bd3ed2e457cf1ad
Diogo <diogoanonimos@gmail.com>
2025-02-18 09:49:59 +0100

    i hate betty

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,17 +1,26 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * jack_bauer - Prints from 00:00 to 23:59
+ *
+ * Return: None
+ */
 void jack_bauer(void)
 {
-	int current_hour = 0;
-	int current_minute = 0;
+	int current_hour;
+	int current_minute;
 
-	while (current_hour < 24) 
+	current_hour = 0;
+	current_minute = 0;
+	while (current_hour < 24)
 	{
-		printf("%d:%d\n", current_hour, current_minute);
-
+		_putchar((current_hour / 10) + '0');
+		_putchar((current_hour % 10) + '0');
+		_putchar(':');
+		_putchar((current_minute / 10) + '0');
+		_putchar((current_minute % 10) + '0');
+		_putchar('\n');
 		current_minute++;
-		
 		if (current_minute >= 60)
 		{
 			current_minute = 0;

046f5cf5f55e73a61c057fa506c4f816be0beeeb
Diogo <diogoanonimos@gmail.com>
2025-02-19 15:45:12 +0100

    ez no?

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int total = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			total += i;
+		}
+	}
+	printf("%d\n", total);
+}

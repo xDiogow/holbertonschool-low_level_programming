78c0349c6281e788ec9bc86a67b1e4a6c0440b20
Diogo <diogoanonimos@gmail.com>
2025-02-19 13:08:44 +0100

    101-print_comb4

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, j, k;
+	bool first = true;
+	
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i+1; j <= 8; j++)
+		{
+			for (k = j+1; k <= 9; k++)
+			{
+				if (!first)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				
+				first = false;
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

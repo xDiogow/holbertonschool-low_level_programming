0d4aa3676c91887a41aac447edf75c5de2db5146
Diogo <diogoanonimos@gmail.com>
2025-02-19 13:11:06 +0100

    Betty

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -10,19 +10,18 @@ int main(void)
 {
 	int i, j, k;
 	bool first = true;
-	
+
 	for (i = 0; i <= 7; i++)
 	{
-		for (j = i+1; j <= 8; j++)
+		for (j = i + 1; j <= 8; j++)
 		{
-			for (k = j+1; k <= 9; k++)
+			for (k = j + 1; k <= 9; k++)
 			{
 				if (!first)
 				{
 					putchar(',');
 					putchar(' ');
 				}
-				
 				first = false;
 				putchar(i + '0');
 				putchar(j + '0');

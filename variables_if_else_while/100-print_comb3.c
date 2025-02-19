557e493381aa6e3c6ee7e09aa1e24240bdbc2abf
Diogo <diogoanonimos@gmail.com>
2025-02-19 12:27:47 +0100

    bettyyy

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    int i, j;
-    int first = 1;
+	int i, j;
+	int first = 1;
 
-    for (i = 0; i <= 8; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
-        {
-            if (!first)
-                putchar(','), putchar(' ');
-            putchar(i + '0');
-            putchar(j + '0');
-            first = 0;
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			if (!first)
+				putchar(','), putchar(' ');
+			putchar(i + '0');
+			putchar(j + '0');
+			first = 0;
+		}
+	}
+	putchar('\n');
+	return (0);
 }

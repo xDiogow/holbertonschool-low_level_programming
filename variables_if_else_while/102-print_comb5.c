024e10afedaf391284ee5a5aa11dbb1c0c144b85
Diogo <diogoanonimos@gmail.com>
2025-02-19 13:54:18 +0100

    PLSSSS

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -1,36 +1,6 @@
 #include <stdio.h>
 
 /**
- * print_int_helper - Prints a number in the console using the putchar function.
- * @n: Number to print
- *
- * Return: None
- */
-void print_int_helper(int n)
-{
-	if (n / 10)
-		print_int_helper(n / 10);
-
-	putchar(n % 10 + '0');
-}
-
-/**
- * print_int - Prints a number in the console
- * @n: Number to print
- */
-void print_int(int n)
-{
-	if (n < 10)
-	{
-		putchar('0');
-		putchar(n + '0');
-	}
-	else
-		print_int_helper(n);
-}
-
-
-/**
  * main - Entry point
  *
  * Return: Always 0

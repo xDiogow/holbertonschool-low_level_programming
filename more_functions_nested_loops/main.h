ecae571aabceb50b851db8c00e43b852e65161a3
Diogo <diogoanonimos@gmail.com>
2025-02-24 11:46:58 +0100

    101-print_number.c

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,8 +3,6 @@ int _putchar(char);
 /* Project 1 */
 int _isupper(int);
 int _isdigit(int);
-int mul(int, int);
-void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);

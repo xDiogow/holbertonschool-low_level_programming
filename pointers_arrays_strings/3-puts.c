9d9b14b9aef646830ce72a2cecde6610092227a0
Diogo <diogoanonimos@gmail.com>
2025-02-24 13:35:49 +0100

    3-puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,13 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: String
+ */
 void _puts(char *str)
 {
-	int i = 0;
-
 	while (*str != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(*str);
 	}
+	_putchar('\n');
 }

2dde0bad1b0b27c395f0f661c9eb1b2267188ea9
Diogo <diogoanonimos@gmail.com>
2025-02-24 11:56:19 +0100

    101-print_number.c

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -3,10 +3,16 @@
 
 /**
  * print_number - prints a integer
- * @n: the integer
+ * @n: integer
  */
 void print_number(int n)
 {
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
 	if (n / 10)
 		print_number(n / 10);
 	_putchar((n % 10) + '0');

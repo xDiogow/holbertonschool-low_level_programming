94636206da57901cc0b790cd87f807246e9e1d7e
Diogo <diogoanonimos@gmail.com>
2025-02-24 09:52:32 +0100

    pls

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,7 @@ void more_numbers(void)
 
 	for (i = 0; i <= 14; i++)
 	{
-		if (i >= 10)
-		{
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
-		}
-		else
-			_putchar(i + '0');
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }

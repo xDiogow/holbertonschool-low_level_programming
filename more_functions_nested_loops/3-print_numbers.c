88abe1520518d10b1e3497bb0d5f56f2ace029a9
Diogo <diogoanonimos@gmail.com>
2025-02-24 09:35:37 +0100

    BETTYYYYYYYYYy

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,12 +2,13 @@
 #include "main.h"
 
 /**
- * print_numbers - Write a function that prints the numbers, from 0 to 9, followed by a new line.
+ * print_numbers - Prints the numbers, from 0 to 9, followed by a new line.
  * Return: 0
  */
-void print_numbers()
+void print_numbers(void)
 {
 	int i;
+
 	for (i = 0; i <= 9; i++)
 		_putchar(i + '0');
 	_putchar('\n');

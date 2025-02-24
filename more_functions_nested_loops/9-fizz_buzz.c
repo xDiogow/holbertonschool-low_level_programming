a2c82019b430c10e34fd5ba52656e65ab1b5dcef
Diogo <diogoanonimos@gmail.com>
2025-02-24 10:41:23 +0100

    FIZZBUZZ

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+void fizz_buzz()
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		printf("\n");
+	}
+}

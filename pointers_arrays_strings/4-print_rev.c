72f4bd922e319167ba6cdb03f6293345b4a646d9
Diogo <diogoanonimos@gmail.com>
2025-02-24 14:10:05 +0100

    4-print_rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -3,6 +3,24 @@
 #include "main.h"
 
 /**
+ * _strlen - Counts the amount of char
+ * @str: String
+ * Return: Length of string
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		str++;
+		len++;
+	}
+	return (len);
+}
+
+
+/**
  * print_rev - Prints a string, in reverse
  * @str: String
  */

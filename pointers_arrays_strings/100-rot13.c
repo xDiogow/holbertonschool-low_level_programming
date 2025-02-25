de5ca13c6f5f9db05ebfc77e34e2c8d03ca3f91e
Diogo <diogoanonimos@gmail.com>
2025-02-25 15:08:15 +0100

    100-rot13.c

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - Encodes a string using rot13
+ * @str: input
+ * Return: pointer to str
+ */
+char *rot13(char *str)
+{
+	int i = 0, diff;
+
+	while (str[i] != '\0')
+	{
+		if ((str[i] < 'a' && str[i] > 'z') || (str[i] < 'A' && str[i] > 'Z'))
+		{
+			i++;
+			continue;
+		}
+
+		if (str[i] > 'a' && str[i] < 'z')
+		{
+			if (str[i] <= 'z')
+				str[i] = str[i] + 13;
+			else
+			{
+				diff = 122 - str[i];
+				str[i] = 97 + 13 - diff;
+			}
+		}
+	}
+}

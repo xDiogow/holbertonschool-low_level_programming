5245dfca80a41b4cc907b57925f475538135cd4e
Diogo <diogoanonimos@gmail.com>
2025-02-25 13:28:45 +0100

    5-string_toupper.c

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - Converts a string to uppercase.
+ * @str: String to be changed
+ * Return: ptr to str
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}

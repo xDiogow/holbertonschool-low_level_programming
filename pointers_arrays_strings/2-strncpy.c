7c0b02081d3733c6f4ad35d2ee9c3b478e477a23
Diogo <diogoanonimos@gmail.com>
2025-02-25 12:47:37 +0100

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,16 @@
 #include "main.h"
 
+/**
+ * _strncpy - Copies a string
+ * @dest: Destination string
+ * @src: Source string
+ * @n: bytes
+ * Return: Destination
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	for (; i < n; i++)

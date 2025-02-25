39eacc6f935e43aa4d00434554b5e0df50e3f926
Diogo <diogoanonimos@gmail.com>
2025-02-25 15:29:33 +0100

    100-rot13.c

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -10,13 +10,16 @@ char *rot13(char *str)
 {
 	int i = 0, j = 0;
 	char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char rot13[]   = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	while (str[i] != '\0')
 	{
-		for (j = 0; j < 54; j++)
+		for (j = 0; j < 52; j++)
 			if (str[i] == letters[j])
+			{
 				str[i] = rot13[j];
+				break;
+			}
 		i++;
 	}
 	return (str);

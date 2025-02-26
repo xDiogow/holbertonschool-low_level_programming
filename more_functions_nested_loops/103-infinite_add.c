f1601b6485046b0a6d42c4b67a4206aa4a3fef19
Diogo <diogoanonimos@gmail.com>
2025-02-26 14:51:53 +0100

    103-infinite_add.c

---
+++ more_functions_nested_loops/103-infinite_add.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i, j, k, sum, carry;
+	int len1 = strlen(n1);
+	int len2 = strlen(n2);
+
+	i = len1 - 1;
+	j = len2 - 1;
+	k = size_r - 2;
+	carry = 0;
+
+	while (i >= 0 || j >= 0 || carry)
+	{
+		sum = carry;
+		if (i >= 0)
+			sum += n1[i] - '0';
+		if (j >= 0)
+			sum += n2[j] - '0';
+
+		if (k < 0)
+			return (0);
+
+		r[k] = (sum % 10) + '0';
+		carry = sum / 10;
+
+		i--;
+		j--;
+		k--;
+	}
+	
+	memmove(r, r + k + 1, size_r - k - 1);
+	
+	r[size_r - 1] = '\0';
+	return (r);
+}

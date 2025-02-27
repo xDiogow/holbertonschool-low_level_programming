969575c097bda7f3e06451ac2bdf08c66fff0fbc
Diogo <diogoanonimos@gmail.com>
2025-02-27 13:34:29 +0100

    101-print_number.c

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -7,18 +7,23 @@
  */
 void print_number(int n)
 {
-	int num;
+	int n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -(n);
+
+		if (n == -2147483648)
+		{
+			print_number(214748364);
+			_putchar('8');
+			return;
+		}
+		n = -(n);
 	}
-	else
-		num = n;
 
-	if (num / 10)
-		print_number(num / 10);
+	if (n / 10)
+		print_number(n / 10);
 
-	_putchar((num % 10) + '0');
+	_putchar((n % 10) + '0');
 }

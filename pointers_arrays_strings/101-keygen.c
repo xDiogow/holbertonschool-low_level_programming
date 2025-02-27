6d7ebc01300358ccea471e2dbab609d143d026d6
Diogo <diogoanonimos@gmail.com>
2025-02-27 14:46:42 +0100

    101-keygen.c

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,12 +1,39 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_SUM 2772
 
 int main(void)
 {
-	int i;
+    char password[100];
+    int sum = 0, i = 0, random;
 
-	for (i = 0; i <= 22; i++)	
-		printf("~");
+    srand(time(NULL));
 
-	return (0);
+    while (sum < PASSWORD_SUM - 122)
+    {
+        random = rand() % 94 + 33;
+        password[i] = random;
+        sum += random;
+        i++;
+    }
+
+    random = PASSWORD_SUM - sum;
+    if (random > 126)
+    {
+        password[i] = random / 2;
+        i++;
+        password[i] = random - password[i - 1];
+    }
+    else
+    {
+        password[i] = random;
+    }
+    i++;
+    password[i] = '\0';
+
+    printf("%s", password);
+
+    return (0);
 }

acefaa84dd48b9b80a03c1516dab350bbf48dd01
Diogo <diogoanonimos@gmail.com>
2025-03-03 12:08:08 +0100

    4-pow_recursion.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - Puts a number to power of y
+ * @x: Value
+ * @y: Power
+ * Return: Value of x raised to power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	int i;
+	int sum = x;
+
+	for (i = 0; i < y; i++)
+	{
+		sum *= x
+	}
+	return (sum);
+}

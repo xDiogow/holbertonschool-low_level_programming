162fee6b5c50e6a7fafe58b1fd407ab31f5b3b38
Diogo <diogoanonimos@gmail.com>
2025-03-03 10:44:34 +0100

    4-strpbrk.c

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -9,19 +9,17 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0, j = 0;
+	int i, j;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (accept[j] != '\0')
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
-				return (&s[i]);
+				return (s + i);
 			}
-			j++;
 		}
-		i++;
 	}
 	return (NULL);
 }

535ef6776853bc09bda4c13ac507a1b7687d5b5e
Diogo <diogoanonimos@gmail.com>
2025-03-03 10:13:49 +0100

    3-strspn.c

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strnspn - Gets the length of a prefix substring.
+ * @s: input string
+ * @accept: sete of chars to match
+ * Return: length of a prefix substring
+ */
+unsigned int _strnspn(char *s, char *accept)
+{
+	unsigned int c = 0;
+	int i = 0, j = 0;
+
+	while (s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+				c++;
+		}
+
+		i++;
+		j = 0;
+	}
+	return (c);
+}

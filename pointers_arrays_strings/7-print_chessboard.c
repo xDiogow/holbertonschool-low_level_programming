56f5fd4777c90592a68f2fd6d6bebcec7ff03cda
Diogo <diogoanonimos@gmail.com>
2025-03-03 11:04:01 +0100

    7-print_chessboard.c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,9 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_chessboard - Prints a chess board
+ * @a: Board
+ */
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
+
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)

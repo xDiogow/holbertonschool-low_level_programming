6e7654887287e64ef42598059430941a6339665c
Diogo <diogoanonimos@gmail.com>
2025-03-03 10:21:17 +0100

    3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,15 +10,21 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int c = 0;
 	int i = 0, j = 0;
+	int found = 0;
 
 	while (s[i] != '\0')
 	{
 		while (accept[j] != '\0')
 		{
 			if (s[i] == accept[j])
+			{
+				found = 1;
 				c++;
+			}
 			j++;
 		}
+		if (!found)
+			return (c);
 
 		i++;
 		j = 0;

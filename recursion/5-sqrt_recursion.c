be7d38269e8aa17c8f5c9e6991b420f9834a8c81
Diogo <diogoanonimos@gmail.com>
2025-03-03 12:20:45 +0100

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,14 +3,19 @@
 
 int _sqrt_recursion(int n)
 {
-	return (sqrt_helper(n, 1));
+	if (n < 0)
+		return (-1);
+
+	return (_sqrt_helper(n, 0));
 }
 
-int sqrt_helper(int n, int guess)
+int _sqrt_helper(int n, int guess)
 {
+	if (guess * guess > n)
+		return (-1);
+
 	if (guess * guess == n)
 		return guess;
-	if (guess * guess > n)
-		return guess;
-	return (sqrt_helper(n, guess + 1));
+	
+	return (_sqrt_helper(n, guess + 1));
 }

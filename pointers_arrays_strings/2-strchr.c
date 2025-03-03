9576c35fe2bb641db9fe38214c841345cdfac94b
Diogo <diogoanonimos@gmail.com>
2025-03-03 10:01:16 +0100

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,16 +10,16 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-	
+
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
-			return &s[i];
+			return (&s[i]);
 		i++;
 	}
 
 	if (c == '\0')
-        	return &s[i];
+		return (&s[i]);
 
-	return NULL;
+	return (NULL);
 }

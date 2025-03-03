950295503de29639189288de03b42feb726b0734
Diogo <diogoanonimos@gmail.com>
2025-03-03 12:48:35 +0100

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints amount of args
+ * @argc: Amount of arguments
+ * @argv: Array of arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}

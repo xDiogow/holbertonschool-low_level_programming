a51110dd47fe63ee09d0768e91c0fe1f865f64a6
Diogo <diogoanonimos@gmail.com>
2025-03-03 12:24:05 +0100

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _sqrt_recursion - Finds sqrt of n
+ * @n: Number to find sqrt
+ * Return: Search using the helper
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
@@ -9,13 +14,19 @@ int _sqrt_recursion(int n)
 	return (_sqrt_helper(n, 0));
 }
 
+/**
+ * _sqrt_helper - Helper for sqrt -> AI has been used
+ * @n: Number to find sqrt
+ * @guess: The guess
+ * Return: The sqrt ot keep searching
+ */
 int _sqrt_helper(int n, int guess)
 {
 	if (guess * guess > n)
 		return (-1);
 
 	if (guess * guess == n)
-		return guess;
+		return (guess);
 	
 	return (_sqrt_helper(n, guess + 1));
 }

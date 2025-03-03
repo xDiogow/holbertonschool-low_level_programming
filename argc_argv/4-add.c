79b75bb7c6de368af79738750a9e85ee8f96351a
Diogo <diogoanonimos@gmail.com>
2025-03-03 13:09:29 +0100

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -8,8 +8,12 @@ int main(int argc, char *argv[])
 	for (i = 1; i < argc; i++)
 	{
 		val = atoi(argv[i]);
-		if (val != 0)
-			sum += val;
+		if (val == 0)
+		{
+			printf("Error\n");
+			return;
+		}
+		sum += val;
 	}
 	printf("%d\n", sum);
 	return (0);

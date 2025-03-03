1357f886e7a4b446754319a2f36bbff68e4f447d
Diogo <diogoanonimos@gmail.com>
2025-03-03 12:09:43 +0100

    4-pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -9,12 +9,7 @@
  */
 int _pow_recursion(int x, int y)
 {
-	int i;
-	int sum = x;
-
-	for (i = 0; i < y; i++)
-	{
-		sum *= x;
-	}
-	return (sum);
+	if (y == 0)
+		return 1;
+	return (x * _pow_recursion(x, y - 1));
 }

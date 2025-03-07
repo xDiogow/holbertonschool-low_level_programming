1b939214cdda4aec706c7bcd9deaedbd0b35daac
Diogo <diogoanonimos@gmail.com>
2025-03-07 11:50:42 +0100

    100-change.c

---
+++ argc_argv/100-change.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: RTFM
+ * @argv: RTFM
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	int total, i, coins;
+
+	if (argc < 0)
+	{
+		printf("0\n");
+	}
+
+	if (argc != 1)
+	{
+		printf("Error\n");
+		return 1;
+	}
+
+	total = atoi(argv[1]);
+	coins = 0;
+
+	while (total % 25 == 0)
+	{
+		total -= 25;
+		coins += 1;
+	}
+
+	while (total % 10 == 0)
+	{
+		total -= 10;
+		coins += 1;
+	}
+
+	while (total % 5 == 0)
+	{
+		total -= 5;
+		coins += 1;
+	}
+
+	while (total % 2 == 0)
+	{
+		total -= 2;
+		coins += 1;
+	}
+
+	while (total % 1 == 0)
+	{
+		total -= 1;
+		coins += 1;
+	}
+	printf("%d\n", coins);
+}

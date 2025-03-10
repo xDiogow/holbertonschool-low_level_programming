60ec579192297d0de6fcdcc285786aa55f93979e
Diogo <diogoanonimos@gmail.com>
2025-03-10 12:53:01 +0100

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -10,9 +10,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (nDog == NULL)
 		return (NULL);
 	
-	nDog->name = name;
+	nDog->name = malloc(strlen(name) + 1);
+	if (nDog->name == NULL)
+		return (NULL);
+	nDog->name = strcpy(nDog->name, name);
+
 	nDog->age = age;
-	nDog->owner = owner;
+
+	nDog->owner = malloc(strlen(owner) + 1);
+	if (nDog->owner == NULL)
+		return (NULL);
+	nDog->owner = strcpy(nDog->owner, owner);
 
 	return (nDog);
 }

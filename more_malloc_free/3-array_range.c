15423eded046c6c9c66f4fe75e9294324e699cc8
Diogo <diogoanonimos@gmail.com>
2025-03-10 11:58:52 +0100

    3-array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ptr = malloc(sizeof(int) * max);
+	ptr = malloc(sizeof(int) * (max - min));
 	if (ptr == NULL)
 		return (NULL);
 

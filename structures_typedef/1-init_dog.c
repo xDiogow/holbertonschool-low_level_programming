f2dc3a834c95a05bd6eacae84ee12ed8cc92e5b9
Diogo <diogoanonimos@gmail.com>
2025-03-10 12:22:03 +0100

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void init_dog(struct dog *d, char *name, char *name, float age, char *owner)
+{
+	*d->name = name;
+	*d->age = age;
+	*d->owner = owner;
+}

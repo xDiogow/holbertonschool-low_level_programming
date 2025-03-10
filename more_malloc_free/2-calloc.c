110b7329748faca1a3be151c6c8cb43f6803b62b
Diogo <diogoanonimos@gmail.com>
2025-03-10 11:26:31 +0100

    2-calloc.c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - Just like malloc but initializes to 0
+ * @nmemb: Number of elements
+ * @size: Size of a element
+ * Return: ptr
+ */
+int *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr;
+	int i;
+
+	ptr = = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+	{
+		ptr[i] = 0;
+	}
+
+	return (ptr);
+}

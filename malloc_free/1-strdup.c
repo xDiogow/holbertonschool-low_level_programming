04a0c6b90d4ea13b5d41f3b2985b631e097bb60f
Diogo <diogoanonimos@gmail.com>
2025-03-10 09:51:51 +0100

    1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,13 +11,10 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	while (*str != '\0')
-	{
+	while (str[size] != '\0')
 		size++;
-		str++;
-	}
 
-	ptr = malloc(sizeof(char) * size);
+	ptr = malloc(sizeof(char) * (size + 1));
 	if (ptr == NULL)
 		return (NULL);
 

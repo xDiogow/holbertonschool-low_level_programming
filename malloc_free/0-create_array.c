ba775377e474b828a15132c888a5926683c1b367
Diogo <diogoanonimos@gmail.com>
2025-03-10 09:30:35 +0100

    0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -5,9 +5,13 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *ptr = malloc(sizeof(char) * size);
+	char *ptr;
 
 	if (size == 0)
+		return NULL;
+
+	ptr = malloc(sizeof(char) * size);
+	if (ptr == NULL)
 		return NULL;
 
 	for (i = 0; i < size; i++)

1b1e7ecc9274fbff029f4446e593c840e422c01b
Diogo <diogoanonimos@gmail.com>
2025-03-10 11:28:21 +0100

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ int *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = = malloc(size * nmemb);
+	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
 		return (NULL);
 

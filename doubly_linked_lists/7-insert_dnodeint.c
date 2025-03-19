1a0e4150318fd6d8682bec4946a26be3634120b6
Diogo <diogoanonimos@gmail.com>
2025-03-19 09:16:44 +0100

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -21,6 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				new_node->prev = (*h)->prev;
 			new_node->next = (*h);
 		}
+		(*h) = (*h)->next;
 	}
 
 	return (new_node);

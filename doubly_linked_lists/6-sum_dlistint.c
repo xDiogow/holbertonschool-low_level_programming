5b15aea4507608bf540c0df5756beacbd4a6ebf2
Diogo <diogoanonimos@gmail.com>
2025-03-19 08:59:29 +0100

    6-sum_dlistint.c

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
